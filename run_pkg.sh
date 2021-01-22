35f6c8f85f9b144ee7993325bfe51d6bd7177fcb
Ian Muchina <49595512+IanMuchina@users.noreply.github.com>
2021-01-22 04:25:28 +0300

    Update tests

--- run_pkg.sh
+++ run_pkg.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-# This script installs apps
+# Installs some essential porgrams
 set -e
 set -x
 
 # Official repos
 sudo apt-get update
 sudo apt-get -y install $(cat ./pkg/apt.txt )
-
-# Snap
-# sudo snap install $(cat ./pkg/snap.txt)
-
-# Pypi
-pip3 install $(cat ./pkg/pip.txt) #Pypi
-
-# RubyGems
-gem install jekyll bundler # Jekyll
+pip3 install $(cat ./pkg/pip.txt)
