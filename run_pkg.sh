e4bf367abb4714dd3ae5b4e7c42b0787509a28bd
Ian Muchina <muchinaofficial@gmail.com>
2021-08-27 09:42:55 +0300

    Delete Files

--- run_pkg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Installs some essential porgrams
-set -e
-set -x
-
-# Official repos
-sudo apt-get update
-sudo apt-get -y install $(cat ./pkg/apt.txt )
-pip3 install $(cat ./pkg/pip.txt)
